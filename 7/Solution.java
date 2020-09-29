73c477146e93cd09a8cf24673ac34273ad72f462
Polina <polski7595@gmail.com>
2020-09-29 08:51:26 +0700

    lab 1 Var 7

---
+++ 7/Solution.java
@@ -0,0 +1,9 @@
+import java.io.PrintStream;
+public class hello
+{
+    public static PrintStream out = System.out;
+    public static void main(String[] args)
+    {
+        out.println("Hello, world!");
+    }
+}
