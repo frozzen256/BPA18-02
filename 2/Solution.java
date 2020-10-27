8e25f6a9d9c463267bbf27b46be346b1e89fdd92
alexey <maksss071@gmail.com>
2020-10-27 12:35:09 +0700

    Что то новоеё (солюшен поменял)

--- 2/Solution.java
+++ 2/Solution.java
@@ -15,20 +15,24 @@ public class Main {
 
     // Complete the countApplesAndOranges function below.
     static int countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+       public static int countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
         int result =0;
 
         for(int i=0;i<apples.length;i++)
         {
-            if(apples[i]>=s && apples[i]<=t)
+            apples[i]=apples[i]+a;
+            if(apples[i]>s && apples[i]<t)
                 result++;
         }
         for(int j=0;j<oranges.length;j++)
         {
-            if(oranges[j]>=s && oranges[j]<=t)
+            oranges[j]=oranges[j]+b;
+            if(oranges[j]>s && oranges[j]<t)
                 result++;
         }
 
         return result;
+    }
     }
 
     private static final Scanner scanner = new Scanner(System.in);
