844e68684c6d1f24f865b08084584a3551fc9925
nadiabenko <nadiabenkoo@gmail.com>
2020-11-03 11:54:06 +0700

    KUnit4

--- 3/KUnit/src/main/java/Program.java
+++ 3/KUnit/src/main/java/Program.java
@@ -3,6 +3,26 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+class JThread extends Thread {
+
+    private int x1,v1,x2,v2;
+    JThread(int x1, int v1, int x2, int v2){
+        this.x1 = x1;
+        this.v1 = v1;
+        this.x2 = x2;
+        this.v2 = v2;
+    }
+
+    public void run(){
+
+        System.out.printf("%s старт... \n", Thread.currentThread().getName());
+
+        System.out.println("Результат вычисления: " + Solution.kangaroo(x1, v1, x2, v2));
+
+        System.out.printf("%s финиш... \n", Thread.currentThread().getName());
+    }
+}
+
 public class Program {
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -27,6 +47,9 @@ public class Program {
         bufferedWriter.newLine();
 
         bufferedWriter.close();
+
+        new JThread(x1, v1, x2, v2).start();
+        new JThread(x1, v1, x2, v2).start();
 
         scanner.close();
 
