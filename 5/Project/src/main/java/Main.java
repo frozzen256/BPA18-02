afad51c232126b617e0e3fa2d2017f9f91139f25
Serfeg <mirrorsnight@gmail.com>
2020-12-01 13:29:34 +0700

    Ещё раз потоки

---
+++ 5/Project/src/main/java/Main.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+        SockSort sockSort = new SockSort();
+        //int r = sockSort.sockMerchant(n,ar);
+        //System.out.println(r);
+        //bufferedWriter.write(String.valueOf());
+        //bufferedWriter.newLine();
+
+        //bufferedWriter.close();
+        System.out.println("Проверяем");
+        System.out.println("Старт "+Thread.currentThread().getName());
+        new Thread(() -> System.out.println("Результат "+ sockSort.sockMerchant(n,ar))).start();
+        new Thread(() -> System.out.println("Результат "+ sockSort.sockMerchant(n,ar))).start();
+        System.out.println("Стоп "+Thread.currentThread().getName());
+        System.out.println();
+        scanner.close();
+    }
+}
