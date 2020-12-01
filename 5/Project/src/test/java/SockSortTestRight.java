9525cd7c98f51e8367b1f3865886569990f01739
Serfeg <mirrorsnight@gmail.com>
2020-12-01 13:21:14 +0700

    ...

--- 5/Project/src/test/java/SockSortTestRight.java
+++ 5/Project/src/test/java/SockSortTestRight.java
@@ -1,9 +1,11 @@
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 
-public class SockSortTestRight {
+import static org.junit.jupiter.api.Assertions.*;
 
-    @org.junit.Test
-    public void sockMerchant() {
+class SockSortTestRight {
+
+    @Test
+    void sockMerchant() {
         SockSort sockSort = new SockSort();
         int actual = sockSort.sockMerchant(9,new int[]{10, 20, 20, 10, 10, 30, 50, 10, 20});
         int expected = 2;
--- 5/Project/src/test/java/SockSortTestWrong.java
+++ 5/Project/src/test/java/SockSortTestWrong.java
@@ -1,11 +1,11 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class SockSortTestWrong {
+class SockSortTestWrong {
 
     @Test
-    public void sockMerchant() {
+    void sockMerchant() {
         SockSort sockSort = new SockSort();
         int actual = sockSort.sockMerchant(5, new int[]{30, 40, 25, 77, 1});
         int expected = 2;
