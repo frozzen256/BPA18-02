a64ceb32a88aea6eefc81552bd07c2c38600b7c3
Serfeg <mirrorsnight@gmail.com>
2020-12-01 13:10:56 +0700

    Обратка

--- 4/Solution.java
+++ 4/Solution.java
@@ -4,12 +4,14 @@ import java.security.*;
 import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
+
 import java.util.regex.*;
 
 public class Solution {
 
     // Complete the staircase function below.
     static void staircase(int n) {
+
 
 
     }
@@ -25,3 +27,4 @@ public class Solution {
         scanner.close();
     }
 }
+
--- Solution.java
+++ Solution.java
@@ -1,9 +1,5 @@
 <<<<<<< HEAD
-<<<<<<< HEAD
 qqqqqqq
-=======
-Ya ne bydy rabotat
->>>>>>> parent of f8ac374... Delete
 =======
 Ya ne bydy rabotat
 >>>>>>> parent of f8ac374... Delete
