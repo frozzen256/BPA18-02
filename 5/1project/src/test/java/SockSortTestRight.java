73cfc026a5fd6a88740f0db4960216873f489ae1
Serfeg <mirrorsnight@gmail.com>
2020-12-01 12:37:18 +0700

    Восстановление лабораторной работы

--- 5/1project/src/test/java/SockSortTestRight.java
+++ 5/1project/src/test/java/SockSortTestRight.java
@@ -1,11 +1,9 @@
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class SockSortTestRight {
 
-class SockSortTestRight {
-
-    @Test
-    void sockMerchant() {
+    @org.junit.Test
+    public void sockMerchant() {
         SockSort sockSort = new SockSort();
         int actual = sockSort.sockMerchant(9,new int[]{10, 20, 20, 10, 10, 30, 50, 10, 20});
         int expected = 2;
--- 5/1project/src/test/java/SockSortTestWrong.java
+++ 5/1project/src/test/java/SockSortTestWrong.java
@@ -1,11 +1,11 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class SockSortTestWrong {
+public class SockSortTestWrong {
 
     @Test
-    void sockMerchant() {
+    public void sockMerchant() {
         SockSort sockSort = new SockSort();
         int actual = sockSort.sockMerchant(5, new int[]{30, 40, 25, 77, 1});
         int expected = 2;
